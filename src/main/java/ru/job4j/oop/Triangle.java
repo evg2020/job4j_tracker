acd214140cb76181ff8aa9c52543789d78e19a75
Администратор <evg_study@list.ru>
2020-07-02 18:57:27 +0400

    2.2. Рефакторинг - Площадь треугольника. [#299292]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -1,5 +1,39 @@
 package ru.job4j.oop;
-
+// 2.2. Рефакторинг - Площадь треугольника. [#299292]
 public class Triangle {
-    private Point fist;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.first = a;
+        this.second = b;
+        this.third = c;
+    }
+
+    public double period(double a, double b, double c) {
+         double p = (a + b + c) / 2;
+         return p;
+    }
+
+    public boolean exist(double a, double b, double c) {
+                boolean exist = false;
+            if ((a + b) > c && (a + c) > b && (c + b) > a) {
+                exist = true;
+            }
+            return exist;
+
+    }
+
+    public double area() {
+        double res = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            res = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return res;
+    }
 }
---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void period() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double res = triangle.area();
+        assertThat(res, closeTo(8, 0.01));
+    }
+}
