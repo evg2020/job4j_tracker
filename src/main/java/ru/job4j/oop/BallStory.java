31e4900ea9b93e89674d2fe1773c7c56fdee6538
Администратор <evg_study@list.ru>
2020-07-02 09:05:44 +0400

    1.6. Взаимодействие объектов. [#299299]

---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Fox fox = new Fox();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        GoThrow enimals = new GoThrow();
+        fox.tryEat(ball);
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        ball.goThrow(enimals);
+    }
+}
---
+++ src/main/java/ru/job4j/oop/GoThrow.java
@@ -0,0 +1,5 @@
+package ru.job4j.oop;
+
+public class GoThrow {
+    public void enimals(Hare hare, Wolf wolf, Fox fox);
+}
