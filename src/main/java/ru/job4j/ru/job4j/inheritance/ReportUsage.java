dd654c77cf3ba2224cd126a8adfeb4442f49b971
Администратор <evg_study@list.ru>
2020-07-03 19:05:55 +0400

    5. Аннотация @Override [#299309]

--- src/main/java/ru/job4j/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/ru/job4j/inheritance/ReportUsage.java
@@ -1,6 +1,7 @@
 package ru.job4j.ru.job4j.inheritance;
 
 public class ReportUsage {
+
     public static void main(String[] args) {
         TextReport  report = new JSONReport();
         String text = report.generate(" name", " body");
