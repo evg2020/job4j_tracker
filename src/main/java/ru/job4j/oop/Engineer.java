6b9427db55226c8e8563752dece9a34721dd6555
Администратор <evg_study@list.ru>
2020-07-03 14:26:17 +0400

    3. Реализация профессий в коде [#299311]

--- src/main/java/ru/job4j/oop/Engineer.java
+++ src/main/java/ru/job4j/oop/Engineer.java
@@ -2,13 +2,13 @@ package ru.job4j.oop;
 
 public class Engineer extends Profession {
 
+    public Profession(String name, String surname, boolean education, String birthday) {
+        super(name, surname, education, birthday);
+    }
 
-        public boolean getEducation() {
-            return this.education;
-        }
 
     public static void main(String[] args) {
         Engineer eng01 = new Engineer("SPBGTU");
-        System.out.println(eng01.getEducation());
+        System.out.println(eng01.getName());
     }
 }
