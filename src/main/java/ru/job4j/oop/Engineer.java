53c609f6bb025af08fe0f0cb322e9a9a87b38451
Администратор <evg_study@list.ru>
2020-07-03 18:32:28 +0400

    . Переопределение [#299308]

--- src/main/java/ru/job4j/oop/Engineer.java
+++ src/main/java/ru/job4j/oop/Engineer.java
@@ -1,14 +1,21 @@
 package ru.job4j.oop;
 
 public class Engineer extends Profession {
+    private String jobName;
 
-    public Profession(String name, String surname, boolean education, String birthday) {
+    public Engineer(String name, String surname, boolean education, String birthday,String job) {
         super(name, surname, education, birthday);
+        this.jobName = job;
+    }
+
+    public String getJobName() {
+        return this.jobName;
     }
 
 
     public static void main(String[] args) {
-        Engineer eng01 = new Engineer("Jeny", "Ivanova", true, "01.01.2200");
+        Engineer eng01 = new Engineer("Evgenii", "Ivanova", true, "01.01.2200", "Programmer");
         System.out.println(eng01.getName());
+        System.out.println(eng01.getJobName());
     }
 }
