9bd663877da8a6aef39c468a657079b5d76923ac
Администратор <evg_study@list.ru>
2020-07-06 00:43:33 +0400

    5. Tracker - хранилище [#299320]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,6 @@ public class StartUI extends Tracker {
         item01.setId(234);
         item01.setName("Ivan");
         Tracker array = new Tracker();
-        array.add(Item item);
+        array.add(Item item)
     }
 }
