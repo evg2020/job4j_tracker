75686b4bbda6a95212cc5d6ef69e6f56eb226c2e
evg2020 <evg_study@list.ru>
2020-07-10 10:44:23 +0400

    2.1. Реализация класса StartUI [#299263]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,6 +10,7 @@ public class StartUI {
             System.out.println();
             this.showMenu();
             int select = Integer.valueOf(scanner.nextLine()); // ввести пункт меню
+
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
                 System.out.print("Enter name: ");
@@ -22,42 +23,70 @@ public class StartUI {
             } else if (select == 1) {
                 System.out.println("===All items===");
                 for (int i = 0; i < tracker.findAll().length; i++) {
-                    System.out.println("Name: " + tracker.getItems()[i].getName() + "...id:" + tracker.getItems()[i].getId());
+                    String name = tracker.findAll()[i].getName();
+                    int id = tracker.findAll()[i].getId();
+                    System.out.println("Name: " + name + "...id:" + id);
                 }
 
             } else if (select == 2) {
                 System.out.println("===Edit items===");
                 System.out.print("Enter Id: ");
                 int id = Integer.valueOf(scanner.nextLine());
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item();
-                item.setName(name);
-                boolean replace = tracker.replace(id, item);
-                System.out.println("Item for " + name + " was changed : true or false: " + replace);
+                Item idFind = tracker.findById(id);
+                    if (idFind != null ) {
+                        System.out.print("Enter name: ");
+                        String name = scanner.nextLine();
+                        Item item = new Item();
+                        item.setName(name);
+                        boolean replace = tracker.replace(id, item);
+                            if (replace) {
+                                System.out.println("Item for " + name + " had been changed");
+                            } else {
+                                System.out.println("Item NOT changed");
+                            }
+
+                    } else {
+                        System.out.println("Can not find id");
+                    }
 
             } else if (select == 3) {
                 System.out.println("===Delete item===");
                 System.out.print("Enter Id: ");
                 int id = Integer.valueOf(scanner.nextLine());
                 boolean del = tracker.delete(id);
-                System.out.println("Id " + id + " was deleted : true or false: " + del);
+                    if (del) {
+                    System.out.println("Id " + id + " was deleted : true or false: " + del);
+                    } else {
+                    System.out.println("Can not find id");
+                    }
 
             } else if (select == 4) {
                 System.out.println("===Find item by Id===");
                 System.out.print("Enter Id: ");
                 int id = Integer.valueOf(scanner.nextLine());
-                System.out.println(tracker.findById(id).getName());
+                Item idFind = tracker.findById(id);
+                    if (idFind != null ) {
+                    System.out.println(idFind.getName());
+                    } else {
+                    System.out.println("Can not find id");
+                    }
 
             } else if (select == 5) {
                 System.out.println("===Find items by name===");
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
-                for (int i = 0; i < tracker.findByName(name).length; i++) {
-                    System.out.println("Name: " + tracker.findByName(name)[i].getName());
+                Item[] namesEqual = tracker.findByName(name);
+                if (namesEqual != null) {
+                    for (int i = 0; i < namesEqual.length; i++) {
+                        System.out.println("Name: " + namesEqual[i].getName());
+                    }
+                } else {
+                    System.out.println("Can not find id");
                 }
+
             } else if (select == 6) {
                 run = false;
+
             }
         }
     }
