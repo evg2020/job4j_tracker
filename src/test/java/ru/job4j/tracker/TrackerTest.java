05a14c09cf8f3bd70044181014cdaa1841db8bfd
evg2020 <evg_study@list.ru>
2020-07-13 18:24:55 +0400

    5.1. Тестирование. Подготовка данных. [#299261]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
+
     @Test
     public void whenReplace() {
         Tracker tracker = new Tracker();
