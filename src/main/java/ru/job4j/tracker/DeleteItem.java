3a820842f9d1d02a70e61c29ada1ea081ef3a285
evg2020 <evg_study@list.ru>
2020-07-16 14:20:18 +0600

    9.1 Рефакторинг теста @Before @After [#299250]

--- src/main/java/ru/job4j/tracker/DeleteItem.java
+++ src/main/java/ru/job4j/tracker/DeleteItem.java
@@ -2,6 +2,12 @@ package ru.job4j.tracker;
 
 public class DeleteItem implements UserAction {
 
+    private final Output out;
+
+    public  CreateItem(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Delete item";
@@ -10,13 +16,13 @@ public class DeleteItem implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
 
-        Output.println("===Delete item===");
+        System.out.println("===Delete item===");
         int id = Integer.valueOf(input.askInt("Enter Id: "));
         boolean del = tracker.delete(id);
         if (del) {
-            Output.println("Id " + id + " was deleted :" + del);
+            System.out.println("Id " + id + " was deleted :" + del);
         } else {
-            Output.println("Can not find id");
+            System.out.println("Can not find id");
         }
         return true;
     }
