fff2be0045059423699234a1c27c99f6a057dbea
evg2020 <evg_study@list.ru>
2020-07-16 20:53:53 +0600

    9.1 Рефакторинг теста @Before @After [#299250]

---
+++ src/main/java/ru/job4j/ex/FindEISecond.java
@@ -0,0 +1,31 @@
+package ru.job4j.ex;
+
+public class FindEISecond {
+    public  static int indexOf(String[] value, String key) {
+
+        int res = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args)  {
+        ElementNotFoundExeption elem =new ElementNotFoundExeption("There are not any equal char");
+        try {
+            String[] val = {"q", "l", "h", "d"};
+            String key = "0";
+            int res = indexOf(val, key);
+            System.out.println(res);
+        } catch  (ElementNotFoundExeption) {
+//                e.printStackTrace();//
+            System.out.println(elem);
+        }
+    }
+}
+
+
