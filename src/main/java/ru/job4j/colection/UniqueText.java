13309e8831a9045908b69e52ac0ea3dae01c16a5
evg2020 <evg_study@list.ru>
2020-07-31 13:30:45 +0500

    2. Идентичные тексты. [#299245]

--- src/main/java/ru/job4j/colection/UniqueText.java
+++ src/main/java/ru/job4j/colection/UniqueText.java
@@ -1,4 +1,21 @@
 package ru.job4j.colection;
+import java.util.HashSet;
 
 public class UniqueText {
+    public static boolean isEquals(String originText, String dublicateText) {
+        boolean res = true;
+        String[] origin = originText.split(" ");
+        String[] text02 = dublicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String wordOrigin : origin) {
+            check.add(wordOrigin);
+        }
+        for (String word02: text02) {
+            if (check.contains(word02)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
 }
