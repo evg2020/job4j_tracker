6eb5d447894764b730a1cd04cb0bfe9533359a90
evg2020 <evg_study@list.ru>
2020-08-01 13:34:01 +0500

    0. Map, HashMap [#299239]

---
+++ src/main/java/ru/job4j/UseMap.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Set;
+
+public class UseMap {
+    public static void main(String[] args) {
+
+    HashMap<String, String> map = new HashMap<>();
+    map.put("key01", "value01");
+    map.put("parsentev@yandex.ru", "Petr Arsentev");
+    map.put("evg@yandex.ru", "Evg");
+    map.put("parsentev@yandex.ru", "Petr Arsentev");
+    map.remove("key01");
+
+    for (String key : map.keySet()) {
+        String val = map.get(key);
+        System.out.println(key + " =  " + val);
+    }
+
+
+    }
+
+}
