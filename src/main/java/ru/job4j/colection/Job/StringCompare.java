f1404e12c844b54c25e846d316a0def5a8a3647e
evg2020 <evg_study@list.ru>
2020-08-20 00:32:43 +0500

    0. Checkstyle с Maven [#299220]

--- src/main/java/ru/job4j/colection/Job/StringCompare.java
+++ src/main/java/ru/job4j/colection/Job/StringCompare.java
@@ -6,7 +6,7 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int res = -1;
+        int res = 0;
         int left01 = left.length();
         int right01 = right.length();
         int minNumber = Math.min(left01, right01);
@@ -14,8 +14,9 @@ public class StringCompare implements Comparator<String> {
             res = Character.compare(left.charAt(i), right.charAt(i));
             if (res != 0) {
                  break;
+            } else {
+                res = left01 - right01;
             }
-            res = left01 - right01;
 
         }
 
