35d3ca6b8a105fe4ecd6443a3abf725c0c762141
evg2020 <evg_study@list.ru>
2020-09-29 01:04:18 +0500

    3. Ссылки на методы [#299080]

---
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,24 @@
+package ru.job4j.lambda;
+//. Нужно упростить вызов лямбда за счет оператор (::);
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+  }
+    }
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Ivan", "Vanya Petrovich");
+//        Consumer<String> out = (name) -> cutOut(name)
+  Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+}
---
+++ src/main/java/ru/job4j/lambda/Time.java
@@ -0,0 +1,14 @@
+package ru.job4j.lambda;
+import java.util.Comparator;
+import java.util.Arrays;
+
+public class Time {
+    public static void main(String[] args) {
+        String[] names = {"Ivan", "Vanya"};
+        Comparator<String> comp =(left, right) -> {
+            System.out.println("execute comparetor");
+            return left.length()-right.length();
+        };
+        Arrays.sort(names, comp);
+    }
+}
