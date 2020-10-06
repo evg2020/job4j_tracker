6bca5f70b34025f09676d2d2c5136a052ef7fb60
evg2020 <evg_study@list.ru>
2020-10-06 18:03:10 +0300

    0. Stream API [#299090]

---
+++ src/main/java/ru/job4j/lambda/Demo.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.function.Function;
+
+public class Demo {
+    public static void main(String[] args) {
+        Function<String, String> f1 = s -> s + "1";
+        Function<String, String> f2 = s -> s + "2";
+        Function<String, String> f3 = s -> s + "3";
+        Function<String, String> f4 = s -> s + "4";
+        System.out.println(f1.andThen(f2).compose(f3).compose(f4).apply("ABCD"));
+        System.out.println(f1.andThen(f2).andThen(f3).apply("ABCD"));
+    }
+
+
+}
---
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -0,0 +1,25 @@
+package ru.job4j.lambda;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+/*
+
+        1. Создайте ArrayList. Заполните его числами Integer.
+        2. Получите из ArrayList поток данных.
+        Отфильтруйте поток так, чтобы в нем остались
+        только положительные числа.
+        3. Отфильтрованный поток запишите в List.
+       */
+public class StreamUsage {
+    public static void main(String[] args) {
+        ArrayList<Integer> lists = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+        System.out.println(lists);
+        List<Integer> filtsPositive = lists.stream().filter(
+                list -> list %2 == 0
+        ). collect(Collectors.toList());
+        System.out.println(filtsPositive);
+    }
+}
