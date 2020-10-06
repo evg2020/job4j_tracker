788258efdc530083b6dfaefbc0b7367fa7d4f977
evg2020 <evg_study@list.ru>
2020-10-06 18:24:09 +0300

    6. Функции высшего порядка [#299078]

--- src/main/java/ru/job4j/lambda/PhoneDictionary.java
+++ src/main/java/ru/job4j/lambda/PhoneDictionary.java
@@ -13,10 +13,15 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         Predicate<Person> findByName = (Person person) -> person.getName().contains(key);
         Predicate<Person> findBySurname = (Person person) -> person.getSurname().contains(key);
-        Predicate<Person> combine =(Person person) -> findByName.test(person) || findBySurname.test(person);
+        Predicate<Person> findByPhone = (Person person) -> person.getPhone().contains(key);
+        Predicate<Person> findByAddress = (Person person) -> person.getAddress().contains(key);
+        Predicate<Person> combine = (Person person) -> findByName.test(person)
+                || findBySurname.test(person)
+                || findByPhone.test(person)
+                || findByAddress.test(person);
         ArrayList<Person> result = new ArrayList<Person>();
         for (Person person : persons) {
-            if (combine.test(person)) {
+            if (combine.test(person) ) {
                 result.add(person);
             }
         }
--- src/main/java/ru/job4j/lambda/StreamUsage.java
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -18,9 +18,9 @@ public class StreamUsage {
     public static void main(String[] args) {
         ArrayList<Integer> lists = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
         System.out.println(lists);
-        List<Integer> filtsPositive = lists.stream().filter(
-                list -> list % 2 == 0
-        ).collect(Collectors.toList());
+        List<Integer> filtsPositive = lists.stream().
+                filter(list -> list % 2 == 0).
+                collect(Collectors.toList());
         System.out.println(filtsPositive);
     }
 }
